e8a6cc57c3b7deee12162b6c98a425f92b22ad0b
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-01-10 02:23:21 +0600

    Add program to sum two integers
    Implement a program to sum two integers and print the result.

---
+++ Online_Judges/BeeCrowd/Beginner/1001.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    int A, B, X;
+    scanf("%d", &A);
+    scanf("%d", &B);
+    X = A + B; 
+    printf("X = %d\n", X); 
+    return 0;
+}
+
+
+
+// Problem: https://judge.beecrowd.com/en/problems/view/1001
+// Difficulty: Beginner
+// Tags: math
+// Status: Solved
