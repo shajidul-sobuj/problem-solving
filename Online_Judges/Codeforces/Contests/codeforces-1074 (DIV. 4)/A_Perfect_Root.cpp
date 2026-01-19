037c2ec54c725c6d7b25ae8c629f676186d2ad10
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-01-19 12:43:56 +0600

    Update A_Perfect_Root.cpp

--- Online_Judges/Codeforces/Contests/codeforces-1074 (DIV. 4)/A_Perfect_Root.cpp
+++ Online_Judges/Codeforces/Contests/codeforces-1074 (DIV. 4)/A_Perfect_Root.cpp
@@ -1,3 +1,9 @@
+// Contest: Codeforces Round 1074
+// Link: https://codeforces.com/contest/1074/problem/A
+// Author: Shajidul Islam Sobuj
+// Codeforces: https://codeforces.com/profile/shajidul-sobuj
+// GitHub: https://github.com/shajidul-sobuj
+
 #include <bits/stdc++.h>
 using namespace std;
  
