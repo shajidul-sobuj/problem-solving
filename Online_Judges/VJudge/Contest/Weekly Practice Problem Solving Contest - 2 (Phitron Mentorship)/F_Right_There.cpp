0d6306b94bb62fdda5d7c99129641e6aab7c5f00
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-01-21 22:01:50 +0600

    Add files via upload

---
+++ Online_Judges/VJudge/Contest/Weekly Practice Problem Solving Contest - 2 (Phitron Mentorship)/F_Right_There.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n , x;
+        cin >> n >> x;
+        if (n>x)
+            cout << "NO\n";
+        else cout << "YES\n";
+
+    }
+    return 0;
+}
