40935b45d4a7dc7d3e7e8c63104859a5e2bf5b23
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-02-09 00:18:28 +0600

    Add header comment with problem link
    Added a header comment with problem link for A_Domino_piling.

---
+++ Online_Judges/Codeforces/Problemset/A_Domino_piling.cpp
@@ -0,0 +1,17 @@
+/*
+        __________________
+        |                |
+        |    KzSobuj     |
+        |________________|
+    problem link: https://codeforces.com/problemset/problem/50/A
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n,m;
+    cin >> n >> m;
+    int ans = (n*m)/2;
+    cout << ans << endl;
+    return 0;
+}
